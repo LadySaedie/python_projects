3fa9f5825c7cdc7cb90282e70e6428cb6bfca4ef
Philip King <ladysaedie@gmail.com>
2025-05-03 14:35:09 +0000

    choice 2

--- game_on.py
+++ game_on.py
@@ -1,17 +1,16 @@
-# Ask for the user's name and store it
 fname = input("What’s your first name? ")
-
-# Print a personalised greeting
 print(f"Hello, {fname}! Welcome to Game On!")
 
-# Start the adventure by describing the scene and asking for a choice
 print(f"{fname}, you’re in a forest. Do you go left or right?")
-
-# Store the user's choice ('left' or 'right') in the variable 'choice'
 choice = input("Type 'left' or 'right': ")
 
 if choice.lower() == "left":
-    print(f"{fname}! You've found the buried treasure chest!")
+   print(f"{fname}, you found a chest! Open it or run?")
+   choice2 = input("Type 'open' or 'run': ")
+   if choice2.lower() == "open":
+       print(f"{fname}, it’s full of gold!")
+   else:
+       print(f"{fname}, you escaped safely!")
 elif choice.lower() == "right":
     print(f"{fname}! RUN! A wild wolf is about to eat you!")
 else:
